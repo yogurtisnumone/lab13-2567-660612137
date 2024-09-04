df968d59ab99f4fbf917f746a4bdd1df26e79855
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-09-04 11:36:06 +0700

    8!

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,11 +1,13 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
+import MovieRow  from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
+import { searchInputParams } from "@lib/types";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({params}: searchInputParams) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
+  const processedSearchInput = params.searchInputParams.replaceALL("%20"," ");
 
   /*
   tip2 : Use "includes" string method to check substring
@@ -14,17 +16,30 @@ export default function SearchResultPage() {
   tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
   to convert movie title and searchInput to lower case 
   const filteredMovies = movieDB.filter((movie) =>
-    you code here...
+  you code here...
   );
   */
+
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase())  
+  );
+
+  //tip4 : if no movie found, return an appropriate message
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
       <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          {...movie}
+          number={i + 1}
+        />
+      ))}
     </div>
   );
 }
